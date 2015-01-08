fea7dd0f1580f8a0fbd2c520b09333f1b562b96a
Lizzie Koehler & Clem Freeman <student@epicodus.com>
2015-01-08 15:37:23 -0800

    refactor method to remove unnecessary if statements.

--- lib/weekday_finder.rb
+++ lib/weekday_finder.rb
@@ -5,20 +5,6 @@ class String
     day = split_time.at(1)
     year = split_time.at(2)
     input_time = Time.new(year, month, day)
-    if input_time.monday?()
-      "Monday"
-    elsif input_time.tuesday?()
-      "Tuesday"
-    elsif input_time.wednesday?()
-      "Wednesday"
-    elsif input_time.thursday?()
-      "Thursday"
-    elsif input_time.friday?()
-      "Friday"
-    elsif input_time.saturday?()
-      "Saturday"
-    else
-      "Sunday"
-    end
+    input_time.strftime("%A")
   end
 end
